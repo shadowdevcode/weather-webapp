79faadc7e1d373133f86491990e65610511238af
shadowdevcode <vijay.b.sehgal@gmail.com>
2018-09-11 21:19:45 +0530

    Added weather application using django

---
+++ weather/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class City(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:     # show the plural of city as cities instead of citys
+
+        verbose_name_plural = 'cities'
---
+++ weather/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+import requests
+from .models import City
+from .forms import CityForm
+
+
+def index(request):
+    cities = City.objects.all()
+
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&APPID=21aed4695a23eb72bfa3871f5ef428f8'
+
+    if request.method == 'POST':
+        # add actual request data to form for processing
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+    weather_data = []
+    for city in cities:
+
+        city_weather = requests.get(url.format(city)).json()
+
+        weather = {
+            'city': city,
+            'temperature': city_weather['main']['temp'],
+            'description': city_weather['weather'][0]['description'],
+            'icon': city_weather['weather'][0]['icon']
+        }
+        # Add the data for the current city into our list
+        weather_data.append(weather)
+    context = {'weather_data': weather_data, 'form': form}
+    return render(request, 'weather/index.html', context)    # Returns index.html template
